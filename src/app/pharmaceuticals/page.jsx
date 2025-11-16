2d2d270fe4214a579c92cdfe6ed90f04c1641838
ramohin1236 <mohinr26@gmail.com>
2025-11-16 16:51:49 +0600

    pharmaceutical division page added

--- src/app/pharmaceuticals/page.jsx
+++ src/app/pharmaceuticals/page.jsx
@@ -1,17 +1,218 @@
 import React from "react";
+import { BiInjection } from "react-icons/bi";
+import { FaRegHeart } from "react-icons/fa";
+import { FaMicroscope } from "react-icons/fa";
+import { FaEarthAmericas } from "react-icons/fa6";
+import { HiOutlineUsers } from "react-icons/hi2";
+import { MdOutlineMonitorHeart } from "react-icons/md";
+
+
 export default function Pharmaceuticals() {
+  const items = ["Diabetes Management", "Cardiovascular Health", "Respiratory Care", "Mental Health"];
+
   return (
-    <div className="flex justify-between items-center container mx-auto py-10 px-5 md:px-0">
-      <div className="w-1/2">
-        {/* <h1 className="text-7xl font-bold text-white">Comming Soon</h1> */}
-        <img src="/mask.png" alt="doc pic" className="w-full h-full" />
-        <p className="text-gray-600 text-xl mt-5">
-          We are working hard on this page We will be back soon please visit
-          again
-        </p>
+    <div className="flex flex-col gap-24 justify-between items-center container mx-auto py-10 px-5 md:px-0">
+
+      {/* tag */}
+      <div className="pt-10 flex flex-col justify-center items-center w-full gap-10">
+        <div className="bg-[#136bfb] rounded-3xl py-2 px-4">
+          <p>Pharmaceutical Division</p>
+        </div>
+        <div className="flex flex-col gap-4">
+          <h1 className="text-center font-bold text-3xl lg:text-5xl">Revolutionizing Global Healthcare Access </h1>
+
+          <p className="text-center text-sm md:text-base">
+            RNA Supplies is expanding its mission to address critical medication shortages worldwide.
+          </p>
+        </div>
       </div>
-      <div className="w-1/2 flex justify-end items-end">
-        <img src="./doc2.png" alt="doc pic" className="w-[400px] h-[600px] object-contain" />
+
+
+      {/* key focus area */}
+      <div className="">
+        <div className="flex flex-col gap-12 ">
+          <h3 className="text-center text-3xl font-semibold"> Key Focus Areas </h3>
+
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md: gap-6 w-full">
+            {/* first caard */}
+
+            <div className="border pl-4 pr-12 pt-4 pb-6 rounded-lg w-full">
+              {/* Header */}
+              <div className="flex gap-4 items-center mb-4">
+                <FaRegHeart className="text-xl font-semibold" />
+                <p className="text-xl font-semibold">Common Conditions</p>
+              </div>
+
+              {/* List Section */}
+              <ul className="list-disc pl-6 space-y-2">
+                {items.map((item, index) => (
+                  <li
+                    key={index}
+                    className="p-2 rounded-lg text-sm"
+                  >
+                    {item}
+                  </li>
+                ))}
+              </ul>
+            </div>
+            {/* second caard */}
+
+            <div className="border pl-4 pr-12 pt-4 pb-6 rounded-lg w-full">
+              <div className="flex gap-4 items-center mb-4">
+                <FaMicroscope className="text-xl font-semibold" />
+                <p className="text-xl font-semibold">Rare Diseases</p>
+              </div>
+
+              {/* List Section */}
+              <ul className="list-disc pl-6 space-y-2">
+                {items.map((item, index) => (
+                  <li
+                    key={index}
+                    className="p-2 rounded-lg text-sm"
+                  >
+                    {item}
+                  </li>
+                ))}
+              </ul>
+            </div>
+            {/* first caard */}
+
+            <div className="border pl-4 pr-12 pt-4 pb-6 rounded-lg w-full">
+              {/* Header */}
+              <div className="flex gap-4 items-center mb-4">
+                <FaEarthAmericas className="text-xl font-semibold" />
+                <p className="text-xl font-semibold">Global Access</p>
+              </div>
+
+              {/* List Section */}
+              <ul className="list-disc pl-6 space-y-2">
+                {items.map((item, index) => (
+                  <li
+                    key={index}
+                    className="p-2 rounded-lg text-sm"
+                  >
+                    {item}
+                  </li>
+                ))}
+              </ul>
+            </div>
+
+          </div>
+        </div>
+      </div>
+
+
+      {/* Our Process */}
+      <div className="border p-4 rounded-2xl py-12">
+        <div className="flex flex-col gap-12 ">
+          <h3 className="text-center text-3xl font-semibold"> Our Process</h3>
+
+          <div className="flex flex-wrap md: gap-6 w-full">
+            {/* first caard */}
+
+            <div className="w-64">
+              {/* Header */}
+              <div className="flex flex-col gap-2 items-center mb-4">
+                <FaRegHeart className="text-xl font-semibold" />
+                <p className="text-xl font-semibold">Partnership</p>
+                <p className="text-sm text-center">Collaborate with governments and healthcare providers</p>
+              </div>
+
+            </div>
+            {/* first caard */}
+
+            <div className="w-64">
+              {/* Header */}
+              <div className="flex flex-col gap-2 items-center mb-4">
+                <MdOutlineMonitorHeart className="text-xl font-semibold" />
+                <p className="text-xl font-semibold">Assessment</p>
+                <p className="text-sm text-center">Analyze regional healthcare needs and gaps</p>
+              </div>
+
+            </div>
+            {/* first caard */}
+
+            <div className="w-64">
+              {/* Header */}
+              <div className="flex flex-col gap-2 items-center mb-4">
+                <BiInjection className="text-xl font-semibold" />
+                <p className="text-xl font-semibold">Distribution</p>
+                <p className="text-sm text-center">Implement efficient supply chain solutions</p>
+              </div>
+
+            </div>
+            {/* first caard */}
+
+            <div className="w-64">
+              {/* Header */}
+              <div className="flex flex-col gap-2 items-center mb-4">
+                <HiOutlineUsers className="text-xl font-semibold" />
+                <p className="text-xl font-semibold">Impact</p>
+                <p className="text-sm text-center">Monitor and measure healthcare outcomes</p>
+              </div>
+
+            </div>
+
+
+          </div>
+        </div>
+      </div>
+
+      {/* Join Our Mission */}
+      <div className="">
+        <div className="flex flex-col gap-12 ">
+         <div className="flex flex-col justify-center items-center gap-5 max-w-2xl mx-auto">
+             <h3 className="text-center text-3xl font-semibold"> Join Our Mission </h3>
+          <p className="text-center text-sm md:text-base">
+           Together, we can make a significant impact on global healthcare accessibility. Partner with us to bring essential medicines to those who need them most.
+          </p>
+
+          <div className="bg-[#136bfb] rounded-3xl py-2 px-4">
+            <p className="text-center">Contact Our Pharmaceutical Division</p>
+          </div>
+         </div>
+
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md: gap-6 w-full">
+            {/* first caard */}
+
+            <div className="border rounded-lg w-full">
+              {/* Header */}
+              <div className="flex p-12 flex-col justify-center gap-2 items-center mb-4">
+                
+                <p className="text-6xl font-bold ">50+ </p>
+                <p>Countries Served</p>
+              </div>
+
+             
+            </div>
+            {/* Second caard */}
+
+            <div className="border rounded-lg w-full">
+              {/* Header */}
+              <div className="flex p-12 flex-col justify-center gap-2 items-center mb-4">
+                
+                <p className="text-6xl font-bold ">1M+ </p>
+                <p>Lives Impacted</p>
+              </div>
+
+             
+            </div>
+            {/* Third caard */}
+
+            <div className="border rounded-lg w-full">
+              {/* Header */}
+              <div className="flex p-12 flex-col justify-center gap-2 items-center mb-4">
+                
+                <p className="text-6xl font-bold ">200+ </p>
+                <p>Medicine Types</p>
+              </div>
+
+             
+            </div>
+         
+
+          </div>
+        </div>
       </div>
     </div>
   );
