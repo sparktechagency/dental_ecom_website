659828820148cc3edabd68475900e4e9291694bb
ramohin1236 <mohinr26@gmail.com>
2025-11-24 17:07:49 +0600

    payment methods

--- src/components/home/PaymentCards.jsx
+++ src/components/home/PaymentCards.jsx
@@ -1,14 +1,12 @@
 import Image from 'next/image'
 import React from 'react'
 
-// import imagefirst from "../../../public/visaa.png"
-// import american from "../../../public/american.png"
-// import visa from "../../../public/vissa.png"
-// import applepay from "../../../public/apple_pay.png"
-// // import paypal from "../../../public/pyl.png"
-// import maestro from "../../../public/maestro.png"
-// import master from "../../../public/master.png"
-// import gpay from "../../../public/g_pay.png"
+import imagefirst from "../../../public/visaa.png"
+import american from "../../../public/american.png"
+import visa from "../../../public/vissa.png"
+import applepay from "../../../public/apple_pay.png"
+import maestro from "../../../public/maestro.png"
+import master from "../../../public/master.png"
 
 
 
@@ -22,29 +20,23 @@ const PaymentCards = () => {
        data-aos-duration="800"
         className='flex justify-center flex-col items-center py-12 gap-5'>
              <p className='text-2xl font-semibold text-black'>Accepted Payment Methods</p>
-             {/* <Image
-             src={image}
-             height={100}
-             width={100}
-             className='w-[200px] '
             
-             /> */}
 
-           <div className='grid grid-cols-3 md:grid-cols-3 lg:grid-cols-4 gap-12 pt-8'>
-           {/* <div className=' w-12 md:w-24 lg:w-32'> 
+           <div className='grid grid-cols-3 md:grid-cols-3 lg:grid-cols-3 gap-12 pt-8'>
+           <div className=' w-12 md:w-24 lg:w-32'> 
             <Image
             
             src={imagefirst}/>
-             </div> */}
-             {/* <div className='w-12 md:w-24 lg:w-32 '> <Image 
+             </div>
+             <div className='w-12 md:w-24 lg:w-32 '> <Image 
              className='w-24'
              src={american}/> </div>
              <div className='w-12 md:w-24 lg:w-32 '> <Image src={visa}/> </div>
-             <div className='w-12 md:w-24 lg:w-32 '> <Image src={applepay}/> </div> */}
-             {/* <div className='w-12 md:w-24 lg:w-32 '> <Image src={paypal}/> </div> */}
-             {/* <div className='w-12 md:w-24 lg:w-32 '> <Image src={maestro}/> </div>
+             <div className='w-12 md:w-24 lg:w-32 '> <Image src={applepay}/> </div>
+          
+             <div className='w-12 md:w-24 lg:w-32 '> <Image src={maestro}/> </div>
              <div className='w-12 md:w-24 lg:w-32 '> <Image src={master}/> </div>
-             <div className='w-12 md:w-24 lg:w-32'> <Image src={gpay}/> </div> */}
+           
 
 
           
