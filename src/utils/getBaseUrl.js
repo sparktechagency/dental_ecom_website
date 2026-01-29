8b943433d6d972351c041efb290d7a41a7fd4028
ramohin1236 <mohinr26@gmail.com>
2026-01-29 15:43:48 +0600

    aws url added

--- src/utils/getBaseUrl.js
+++ src/utils/getBaseUrl.js
@@ -2,7 +2,7 @@
 
 export const getBaseUrl = () => {
   return 'https://api.rnadental.co.uk';
-  return 'http://13.134.227.59:3005';
+  // return 'http://13.134.227.59:3005';
   // return 'http://10.10.20.22:5050';
   // return 'http://13.134.227.59';
   // return 'http://10.10.20.22:3045';
